5e3b879d0c2fbfce4e762a7e04f90223f7c6d08a
grant-pie <grant-pie@users.noreply.github.com>
2025-03-21 09:41:25 +0000

    deploy: 7fec4a218c8d87001c77fd19a4def312833d1483

---
+++ _nuxt/CW6Zvu__.js
@@ -0,0 +1,1 @@
+import{I as i,J as o,K as u}from"./CpfRM8F9.js";import{u as r}from"./Cl4f5Tkc.js";const d=i(async s=>{let e,a;const t=r();if(t.isAuthenticated||([e,a]=o(()=>t.initAuth()),await e,a()),!t.isAuthenticated)return u("/login")});export{d as default};
